947605262536cad158c98d5789a4256e253d6c78
TheTinyCoder <charawe.yw@gmail.com>
2024-09-01 11:27:17 +0300

    update str & test function

--- models/base_model.py
+++ models/base_model.py
@@ -27,7 +27,7 @@ class BaseModel:
         """
         Returns informal string representation of an instance
         """
-        return f"[{self.__class__.__name__} ({self.id}) {self.__dict__}]"
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}]"
 
     def save(self):
         """
